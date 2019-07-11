5ae1eb35d35578a622244539bb226ce90b8b5d98
Jairo <jairo.314@hotmail.com>
2019-07-11 03:26:49 +0100

    commit 10/07

--- GUI Version 3/Comunicacion.py
+++ GUI Version 3/Comunicacion.py
@@ -1,16 +1,31 @@
+import time 
 import serial
+               
+ser = serial.Serial(            
+    port='/dev/serial0',
+    baudrate = 9600,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS,
+    timeout=1
+ )
 
-arduino = serial.Serial('/dev/tty1', 9600)
-
-print("Starting!")
-
+#txt = b''
+txt2=""
 while True:
-      comando = input('Introduce un comando: ') #Input
-      flagCharacter = 'k'
-      arduino.write(flagCharacter.encode()) #Mandar un comando hacia Arduino
-      if comando == 'H':
-            print('LED ENCENDIDO')
-      elif comando == 'L':
-            print('LED APAGADO')
-
-arduino.close() #Finalizamos la comunicacion
+    txt=b''
+    var = input("Introducir un Comando: ")
+    var+='\r\n'
+    ser.write(var.encode())
+    time.sleep(0.2)
+    while ser.inWaiting() > 0:
+        txt += ser.read()
+    if len(txt)>1:
+        print(txt)
+        #txt2="".join(map(chr,txt))
+        #~ txt2=str(txt, 'utf-8')
+        #~ print('txt2: '+txt2)
+        #~ if(txt2.find("ok")!=-1):
+            #~ print ('It´s OK!')
+    
+    
